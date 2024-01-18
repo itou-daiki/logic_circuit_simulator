0440cc56ae14816053b3f711b5c67f10ea42b78f
伊藤 大貴 <141300506+itou-daiki@users.noreply.github.com>
2024-01-18 12:48:55 +0000

    あ

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,40 +12,39 @@ input1 = st.checkbox("入力 1 (0 または 1)", value=False)
 input2 = st.checkbox("入力 2 (0 または 1)", value=False) if gate != 'NOT' else None
 
 # 結果の計算と画像の表示
-if st.button("計算"):
-    if gate == 'AND':
-        if not input1 and not input2:
-            image = Image.open('AND00.png')
-            result = False
-        elif not input1 and input2:
-            image = Image.open('AND01.png')
-            result = False
-        elif input1 and not input2:
-            image = Image.open('AND10.png')
-            result = False
-        elif input1 and input2:
-            image = Image.open('AND11.png')
-            result = True
-    elif gate == 'OR':
-        if not input1 and not input2:
-            image = Image.open('OR00.png')
-            result = False
-        elif not input1 and input2:
-            image = Image.open('OR01.png')
-            result = True
-        elif input1 and not input2:
-            image = Image.open('OR10.png')
-            result = True
-        elif input1 and input2:
-            image = Image.open('OR11.png')
-            result = True
-    elif gate == 'NOT':
-        if not input1:
-            image = Image.open('NOT0.png')
-            result = True
-        else:
-            image = Image.open('NOT1.png')
-            result = False
+if gate == 'AND':
+    if not input1 and not input2:
+        image = Image.open('AND00.png')
+        result = False
+    elif not input1 and input2:
+        image = Image.open('AND01.png')
+        result = False
+    elif input1 and not input2:
+        image = Image.open('AND10.png')
+        result = False
+    elif input1 and input2:
+        image = Image.open('AND11.png')
+        result = True
+elif gate == 'OR':
+    if not input1 and not input2:
+        image = Image.open('OR00.png')
+        result = False
+    elif not input1 and input2:
+        image = Image.open('OR01.png')
+        result = True
+    elif input1 and not input2:
+        image = Image.open('OR10.png')
+        result = True
+    elif input1 and input2:
+        image = Image.open('OR11.png')
+        result = True
+elif gate == 'NOT':
+    if not input1:
+        image = Image.open('NOT0.png')
+        result = True
+    else:
+        image = Image.open('NOT1.png')
+        result = False
 
     # 画像と計算結果の表示
     st.image(image, caption=f'{gate} gate with input {input1} {"and " + str(input2) if gate != "NOT" else ""}')
